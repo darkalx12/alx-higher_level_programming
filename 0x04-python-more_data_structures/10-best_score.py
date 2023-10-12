b14a79ab67b1c07d3937cf243c172a006b686583
darkalx12 <darkword907@gmail.com>
2023-10-11 19:01:43 -0700

    push

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,6 @@
+#!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary:
-        best_sc = list(a_dictionary.keys())[0]
-        for i in a_dictionary.keys():
-            if a_dictionary[i] > a_dictionary.get(best_sc):
-                best_sc = i
-        return best_sc
+    if not a_dictionary:
+        return (None)
+
+    return (max(a_dictionary, key=a_dictionary.get))
